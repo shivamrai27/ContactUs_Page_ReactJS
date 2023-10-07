575f93e5b01ae8d9b2bd20e56e544e028aba7a13
shivamrai27 <200shivamrai@gmail.com>
2023-10-08 02:43:35 +0530

    Contact Us

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -12,8 +12,30 @@ const ContactForm = () => {
                     <Button text="VIA CALL" icon={<FaPhoneAlt fontSize={"24px"} />} />
                 </div>
                 <Button isOutline={true} text="VIA EMAIL FORM" icon={<HiMail fontSize={"24px"} />} />
+                <form>
+                    <div className={styles.form_control}>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name"></input>
+                    </div>
+                    <div className={styles.form_control}>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email"></input>
+                    </div>
+                    <div className={styles.form_control}>
+                        <label htmlFor="text">Text</label>
+                        <textarea name="text" rows={10}></textarea>
+                    </div>
+                    <div style={{
+                        display: "flex",
+                        justifyContent: "end"
+                    }}>
+                        <Button text="SUBMIT" />
+                    </div>
+                </form>
             </div>
-            <div className={styles.contact_image}> </div>
+            <div className={styles.contact_image}>
+                <img src="./public/images/contact.svg" alt="contact image" />
+            </div>
         </section>
     )
 }
